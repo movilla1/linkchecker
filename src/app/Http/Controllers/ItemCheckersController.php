1d6a1b451bb087ef6ea56652b17c5b1151b66e14
Mario Villarroel <movilla@gmail.com>
2017-09-22 01:03:13 -0300

    Started item checkers

---
+++ src/app/Http/Controllers/ItemCheckersController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ItemCheckers;
+use Illuminate\Http\Request;
+
+class ItemCheckersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ItemCheckers  $itemCheckers
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ItemCheckers $itemCheckers)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ItemCheckers  $itemCheckers
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ItemCheckers $itemCheckers)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ItemCheckers  $itemCheckers
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ItemCheckers $itemCheckers)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ItemCheckers  $itemCheckers
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ItemCheckers $itemCheckers)
+    {
+        //
+    }
+}
