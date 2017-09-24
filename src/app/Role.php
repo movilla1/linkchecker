5a5a8f9962dbd4c01dac0dd655eabc9998696b4e
Juan Pablo Aguilar <aguilarjpb@gmail.com>
2017-09-23 21:09:22 -0300

    Fix wrong argument

--- src/app/Role.php
+++ src/app/Role.php
@@ -9,7 +9,7 @@ class Role extends Model
     public function users()
     {
       return $this
-        ->belongsToMany('User')
+        ->belongsToMany('App\User')
         ->withTimestamps();
     }
 }
--- src/app/User.php
+++ src/app/User.php
@@ -30,7 +30,7 @@ class User extends Authenticatable
     public function roles()
     {
       return $this
-        ->belongsToMany('Role')
+        ->belongsToMany('App\Role')
         ->withTimestamps();
     }
 
