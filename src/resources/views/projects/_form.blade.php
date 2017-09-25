17e5b0279da4873a7526385f0ed004aa21de3dd0
Juan Pablo Aguilar <aguilarjpb@gmail.com>
2017-09-25 00:05:39 -0300

    Implement edit project modal

--- src/resources/views/projects/_form.blade.php
+++ src/resources/views/projects/_form.blade.php
@@ -1,33 +1,30 @@
 
 {!! Form::model($project, array('route' =>$route,'method'=>$method)) !!}
-@if(isset($project->id))
-  <div class="row">
-    <div class="col-md-1">id</div>
-    <div class="col-md-8">{{$project->id}}</div>
-  </div>
-@endif
+  @if(isset($project->id))
+    <div class="row">
+      <div class="col-md-1 offset-md-1">Id</div>
+      <div class="col-md-8">{{$project->id}}</div>
+    </div>
+    <br>
+  @endif
 
   <div class="row">
-    <div class="col-md-1">Title</div>
-    <div class="col-md-8"><input type="text" class="inputbox" size="50" name="title" value="{{$project->title}}"/></div>
+    <div class="col-md-1 offset-md-1">Title</div>
+    <div class="col-md-8"><input type="text" class="inputbox" size="40" name="title" value="{{$project->title}}"/></div>
   </div>
+  <br>
   <div class="row">
-    <div class="col-md-1">Link</div>
-    <div class="col-md-8"><input type="text" class="inputbox" size="60" name="link" value="{{$project->link}}"/></div>
-  </div><br/>
+    <div class="col-md-1 offset-md-1">Link</div>
+    <div class="col-md-8"><input type="text" class="inputbox" size="40" name="link" value="{{$project->link}}"/></div>
+  </div>
+  <br>
+  <br>
   <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-4 offset-md-2">
       <button type="submit" class="btn btn-primary">Save</button>
     </div>
     <div class="col-md-4">
-      <button type="button" class="btn btn-secondary" id="cancel">Cancel</button>
+      <button type="button" class="btn btn-secondary" data-dismiss="modal" id="cancel">Cancel</button>
     </div>
   </div>
-{{ Form::close() }}
-<script type="text/javascript">
-$(document).ready(function(){
-  $("#cancel").click(function(){
-    window.location.href = {{route("projects.index")}};
-  });
-});
 </script>
