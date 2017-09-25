766d804935f5883bf07c40b7664c580563594ebd
Mario Villarroel <movilla@gmail.com>
2017-09-25 19:03:08 -0300

    Almost ready, works but doesn't properly color the box.

--- src/resources/views/itemcheckers/index.blade.php
+++ src/resources/views/itemcheckers/index.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 @section('content')
+<link href="{{URL::asset('css/itemchecker.css')}}" rel="stylesheet" />
 <script type="text/javascript" 
   src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.6/angular.min.js"></script>
 <script type="text/javascript" src="{{URL::asset('js/itemchecker_app.js')}}"></script>
 
 <a href="{{route('projects.create')}}" class="btn btn-default">Add New</a>
-<div class="container-fluid" ng-app='itemChecker' ng-init='projects=<?=$projects?>' ng-controller="itemlist">
+<div class="container-fluid" ng-app='itemChecker' ng-init='projects=<?=$projects?>;statuses=[]' ng-controller="itemlist">
   <div class="row">
     <div class="col-md-12"><h1>Backlinks</h1></div>
   </div>
@@ -14,9 +15,14 @@
         <span class="project-title" ng-if="ProjectName.length>0">Project: <%ProjectName%> - <%ProjectURL%></span>
       </div>
       <div class="col-md-4">
-      Select Project: <select ng-model='projectData' ng-change="LoadLinks(projectData,{{\Auth::user()->id}})" ng-options="project.title for project in projects track by project.id">
+        Select Project: <select ng-model='projectData' ng-change="LoadLinks(projectData,{{\Auth::user()->id}})" ng-options="project.title for project in projects track by project.id">
         </select>
       </div>
+  </div>
+  <div class="row">
+    <div class="col-md-2 right">
+     <button type="button" ng-click="CheckAllLinks()">Refresh statuses</button>
+    </div>
   </div>
   <div class="row" ng-if="!ErrorMsg">
     <div class="col-md-1"><h3>#</h3></div>
@@ -29,8 +35,9 @@
     <div class="col-md-1"><%row.id%></div>
     <div class="col-md-4"><%row.website%></div>
     <div class="col-md-4"><%row.backlink%></div>
-    <div class="col-md-1" style="color: green" ng-if="statuses[row.id]==true"></div>
-    <div class="col-md-1" style="color: red" ng-if="statuses[row.id]==false"></div>
+    <div class="col-md-1">
+      <span ng-style='<% statuses[row.id] %>' class="blank-status">&nbsp;</span>
+    </div>
     <div class="col-md-2">
       <a href="{{route('items.index')}}/<%row.id%>/edit"><span class="icon icon-edit"></span></a>
       <a href="{{route('items.index')}}/<%row.id%>/delete" data-confirm="This will deactivate this site checking, are you sure?" data-method="delete"><span class="icon icon-delete"></span></a>
@@ -43,4 +50,5 @@
   <div class="col-md-12"><%ErrorMsg%></div>
  </div>
 </div>
+<div ng-bind="statuses"></div>
 @endsection
