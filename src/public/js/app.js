dd1d3fdb85692b171664f3044085e06f90a786bb
Juan Pablo Aguilar <aguilarjpb@gmail.com>
2017-09-26 08:50:12 -0300

    Change items style

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13632,7 +13632,7 @@ itemChecker.controller('itemlist', function ($scope, $http, ListDataService) {
   };
   $scope.CheckAllLinks = function () {
     angular.forEach($scope.ItemData, function (val, idx) {
-      $http.get("api/check_item?row_id=" + val.id).then(function (res) {
+      $http.get("/api/check_item?row_id=" + val.id).then(function (res) {
         color = res.data == '1' ? "status-green" : "status-red";
         $scope.statuses[val.id] = color;
       });
@@ -13649,7 +13649,7 @@ itemChecker.factory('ListDataService', ['$http', '$q', function ($http) {
     getItemData: function getItemData(project, user) {
       var data = $http({
         method: 'GET',
-        url: "api/list_item_json?project_id=" + project + "&uid=" + user
+        url: "/api/list_item_json?project_id=" + project + "&uid=" + user
       });
       return data;
     }
