d57de311601c764c529d202ad27440f83a7deb1e
Juan Pablo Aguilar <aguilarjpb@gmail.com>
2017-09-26 21:20:24 -0300

    Style users form

--- src/resources/views/users/_form.blade.php
+++ src/resources/views/users/_form.blade.php
@@ -22,28 +22,31 @@
   </div>
   <br>
   <div class="row">
-    <div class="col-md-1 offset-md-1">Passowrd</div>
+    <div class="col-md-1 offset-md-1">Password</div>
     <div class="col-md-8">
       {{ Form::text("password",null,["class"=>"inputbox","size"=>"70"])}}
     </div>
   </div>
   <br>
   <div class="row">
-    <div class="col-md-1 offset-md-1">Role</div>
+    <div class="col-md-1 offset-md-1 pt-2">Role</div>
     <div class="col-md-8">
       {{ Form::select('role', array(2 => 'Admin', 1 => 'User'), 1) }}
     </div>
   </div>
   <br>
   <div class="row">
-    <div class="col-md-1 offset-md-1">Status</div>
+    <div class="col-md-1 offset-md-1 pt-2">Status</div>
     <div class="col-md-8">
       {{ Form::select('active', array(1 => 'Active', 0 => 'Blocked'), 1) }}
     </div>
   </div>
+  <br>
+  <br>
+
   <div class="row">
     <div class="col-md-2 offset-md-2">
-     {{Form::submit("Save")}}
+     {{Form::submit("Save", ['class' => 'btn btn-primary'])}}
     </div>
     <div class="col-md-4">
       <a class="btn btn-secondary" href="{{route("users.index") }}" id="cancel">Cancel</a>
