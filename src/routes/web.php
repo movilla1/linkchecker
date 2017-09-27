441f1cfed42ed0f4b02785d57683624659af79db
Juan Pablo Aguilar <aguilarjpb@gmail.com>
2017-09-27 11:14:34 -0300

    Change trash routes role

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,16 +19,16 @@ Route::group(['middleware' => ['auth','role'], 'roles' => ['admin', 'user']], fu
     Route::resource('items',"ItemsController");
     Route::get("/api/list_item_json","ApiController@list_items_json");
     Route::get("/api/check_item","ApiController@check_item");
+});
+
+Route::group(['middleware' => ['auth','role'], 'roles' => ['admin']], function (){
+    Route::resource('users', "UsersController");
     Route::get("/trash","TrashController@manage")->name("trash");
     Route::get("/trash/ui/{item}","TrashController@unerase_item")->name("trash_unerase_item");
     Route::get("/trash/up/{project}","TrashController@unerase_project")->name("trash_unerase_project");
     Route::delete("/trash/empty","TrashController@empty")->name("trash_empty");
     Route::delete("/trash/ri/{item}","TrashController@remove_item")->name("trash_item_remove");
     Route::delete("/trash/rp/{project}","TrashController@remove_project")->name("trash_project_remove");
-});
-
-Route::group(['middleware' => ['auth','role'], 'roles' => ['admin']], function (){
-    Route::resource('users', "UsersController");
 });
 
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
