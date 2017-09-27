501320466d2b42e9119745a0657ff225d26cc30c
Mario Villarroel <movilla@gmail.com>
2017-09-26 23:53:51 -0300

    Trash Controller and operations

--- src/app/Project.php
+++ src/app/Project.php
@@ -8,4 +8,9 @@ class project extends Model
 {
     use SoftDeletes;
     protected $dates = ['deleted_at'];
+
+    public function get_user_name() {
+        $user = User::find($this->user_id);
+        return $user->name;    
+    }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -19,6 +19,12 @@ Route::group(['middleware' => ['auth','role'], 'roles' => ['admin', 'user']], fu
     Route::resource('items',"ItemsController");
     Route::get("/api/list_item_json","ApiController@list_items_json");
     Route::get("/api/check_item","ApiController@check_item");
+    Route::get("/trash","TrashController@manage")->name("trash");
+    Route::get("/trash/ui/{item}","TrashController@unerase_item")->name("trash_unerase_item");
+    Route::get("/trash/up/{project}","TrashController@unerase_project")->name("trash_unerase_project");
+    Route::delete("/trash/empty","TrashController@empty")->name("trash_empty");
+    Route::delete("/trash/ri/{item}","TrashController@remove_item")->name("trash_item_remove");
+    Route::delete("/trash/rp/{project}","TrashController@remove_project")->name("trash_project_remove");
 });
 
 Route::group(['middleware' => ['auth','role'], 'roles' => ['admin']], function (){
