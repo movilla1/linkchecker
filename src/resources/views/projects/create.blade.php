5719d10ea77a57c291a2735d34bec53967748bfb
Juan Pablo Aguilar <aguilarjpb@gmail.com>
2017-09-27 10:38:22 -0300

    Add white background and some padding

--- src/resources/views/projects/create.blade.php
+++ src/resources/views/projects/create.blade.php
@@ -3,7 +3,7 @@
     Create Project
 @endsection
 @section('page-content')
-<div class="container-fluid">
+<div class="container-fluid pt-4 pb-4 bg-white has-shadow">
   @include("projects._form",["route"=>array('projects.store'),"project"=>$project,"method"=>"post"])
 </div>
 @endsection
