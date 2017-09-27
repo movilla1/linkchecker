1062f56c8f2d8be5d8e05746d56af51b8f72c112
Juan Pablo Aguilar <aguilarjpb@gmail.com>
2017-09-27 10:44:56 -0300

    Add div to home

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -4,7 +4,9 @@
 @endsection
 
 @section('page-content')
-<h1>Welcome</h1>
-<p>Please use the option on the sidebar to work with the system.</p>
-<p>If you have any problems, please contact the <a href="mailto: {{env('WEBMASTER_EMAIL')}}">webmaster</a></p>
+<div class="container-fluid pt-4 pb-4 bg-white has-shadow">
+    <h1>Welcome</h1>
+    <p>Please use the option on the sidebar to work with the system.</p>
+    <p>If you have any problems, please contact the <a href="mailto: {{env('WEBMASTER_EMAIL')}}">webmaster</a></p>
+</div>
 @endsection
